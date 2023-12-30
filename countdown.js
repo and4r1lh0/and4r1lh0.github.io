87c6df7fa1f705d3d88951c83dfd0ff9cd7e97c8
KΛRΞN <50175239+and4r1lh0@users.noreply.github.com>
2023-12-30 19:50:39 +0400

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,37 +1,41 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Здесь вы можете установить дату нового года
-    const newYearDate = new Date('January 1, 2024 00:00:00 GMT+00:00').getTime();
+function CountdownTimer(elm,tl,mes){
+ this.initialize.apply(this,arguments);
+}
+CountdownTimer.prototype={
+ initialize:function(elm,tl,mes) {
+  this.elem = document.getElementById(elm);
+  this.tl = tl;
+  this.mes = mes;
+ },countDown:function(){
+  var timer='';
+  var today=new Date();
+  var day=Math.floor((this.tl-today)/(24*60*60*1000));
+  var hour=Math.floor(((this.tl-today)%(24*60*60*1000))/(60*60*1000));
+  var min=Math.floor(((this.tl-today)%(24*60*60*1000))/(60*1000))%60;
+  var sec=Math.floor(((this.tl-today)%(24*60*60*1000))/1000)%60%60;
+  var me=this;
 
-    function updateCountdown() {
-        const currentDate = new Date().getTime();
-        const timeDifference = newYearDate - currentDate;
+  if( ( this.tl - today ) > 0 ){
+   timer += '<span class="number-wrapper"><div class="line"></div><div class="caption">ДНЕЙ</div><span class="number day">'+day+'</span></span>';
+   timer += '<span class="number-wrapper"><div class="line"></div><div class="caption">ЧАСОВ</div><span class="number hour">'+hour+'</span></span>';
+   timer += '<span class="number-wrapper"><div class="line"></div><div class="caption">МИНУТ</div><span class="number min">'+this.addZero(min)+'</span></span><span class="number-wrapper"><div class="line"></div><div class="caption">СЕКУНД</div><span class="number sec">'+this.addZero(sec)+'</span></span>';
+   this.elem.innerHTML = timer;
+   tid = setTimeout( function(){me.countDown();},10 );
+  }else{
+   this.elem.innerHTML = this.mes;
+   return;
+  }
+ },addZero:function(num){ return ('0'+num).slice(-2); }
+}
+function CDT(){
 
-        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+ // Set countdown limit
+ var tl = new Date('2024/01/01 00:00:00');
 
-        // Обновление отображения
-        document.querySelector('.number.day').textContent = days;
-        document.querySelector('.number.hour').textContent = hours;
-        document.querySelector('.number.min').textContent = minutes;
-        document.querySelector('.number.sec').textContent = seconds;
-
-        // Склонение
-        document.querySelector('.caption.day').textContent = getDeclension(days, ['День', 'Дня', 'Дней']);
-        document.querySelector('.caption.hour').textContent = getDeclension(hours, ['Час', 'Часа', 'Часов']);
-        document.querySelector('.caption.min').textContent = getDeclension(minutes, ['Минута', 'Минуты', 'Минут']);
-        document.querySelector('.caption.sec').textContent = getDeclension(seconds, ['Секунда', 'Секунды', 'Секунд']);
-    }
-
-    function getDeclension(number, titles) {
-        const cases = [2, 0, 1, 1, 1, 2];
-        return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
-    }
-
-    // Обновление каждую секунду
-    setInterval(updateCountdown, 1000);
-
-    // Инициализация при загрузке страницы
-    updateCountdown();
-});
+ // You can add time's up message here
+ var timer = new CountdownTimer('CDT',tl,'<span class="number-wrapper"><div class="line"></div><span class="number end">"ВРЕМЯ ВЫШЛО!"</span></span>');
+ timer.countDown();
+}
+window.onload=function(){
+ CDT();
+}
